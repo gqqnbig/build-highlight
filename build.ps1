c447ea882917522f035e04468b72dde9dfa6603e
gqqnbig <gqqnb2005@gmail.com>
2017-05-23 23:55:05 -0700

    添加编译脚本

---
+++ build.ps1
@@ -0,0 +1,12 @@
+﻿cd highlight.js
+node tools\build.js -t browser `
+xml `
+bash `
+autohotkey `
+sql
+
+
+#rm ..\highlight.pack.js
+copy -Force .\build\highlight.pack.js ..\
+#rm ..\styles
+copy -Force .\build\demo\styles ..\
