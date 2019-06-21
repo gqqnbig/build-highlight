92cf6a5ca6dd8e2b35ca5bd49ce6128398a817ec
gqqnbig <gqqnb2005@gmail.com>
2019-06-20 17:31:09 -0700

    优化build，增加plaintext语言

--- build.ps1
+++ build.ps1
@@ -1,18 +1,22 @@
-﻿cd highlight.js
+﻿git submodule update --remote highlight.js
+cd highlight.js
 npm install 
+
 node tools\build.js -t browser `
 autohotkey `
 bash `
 javascript `
 mathematica `
+plaintext `
 sql `
 vbnet `
 xml
 
 
-#rm ..\highlight.pack.js
+
+
+rm ..\highlight.pack.js
 copy -Force .\build\highlight.pack.js ..\
-#rm ..\styles
+rm -Recurse ..\styles
 copy -Force -Recurse .\build\demo\styles ..\
 
-cd ..
